07a12d6194e2fa322171f77bddd8969e3d639602
shivaganesh22 <rockstarshivaganesh@gmail.com>
2024-06-18 19:17:27 +0530

    fix

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.shortcuts import render,redirect
 from .forms import *
 from django.contrib import messages
+
 # Create your views here.
 def home(r):
     return render(r,'index.html')
